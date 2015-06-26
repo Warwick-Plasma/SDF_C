bab0f927ea05cb7f8bd4bda53f5991e33a24d14c
Andrew Wood <andrew@fluidgravity.co.uk>
2015-06-26 13:33:35 +0100

    Add functions sdf_hash_block and sdf_hash_block_list

--- src/sdf_util.c
+++ src/sdf_util.c
@@ -48,4 +48,29 @@ sdf_block_t *sdf_find_block_by_name(sdf_file_t *h, const char *name)
 
     return NULL;
 }
+
+
+void sdf_hash_block(sdf_file_t *h, sdf_block_t *b)
+{
+   sdf_block_t *bb = NULL;
+   HASH_FIND_STR(h->hashed_blocks_by_id, b->id, bb);
+   if ( bb != NULL && b != bb ) {
+      HASH_DELETE(hh, h->hashed_blocks_by_id, bb);
+      bb = NULL;
+   }
+   if ( !bb )
+      HASH_ADD_KEYPTR(hh, h->hashed_blocks_by_id, b->id, strlen(b->id), b);
+}
+
+
+void sdf_hash_block_list(sdf_file_t *h)
+{
+   sdf_block_t *b, *bb;
+
+   b = h->blocklist;
+   while ( b ) {
+      sdf_hash_block(h, b);
+      b = b->next;
+   }
+}
 /**@}*/
