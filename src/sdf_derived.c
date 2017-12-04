37f62bbc48300cf4a4532b8296732faeea1e2f8f
Keith Bennett <K.Bennett@warwick.ac.uk>
2017-12-04 01:22:17 +0000

    Fixed sdf_callback_face_grid for Lagrangian meshes

--- src/sdf_derived.c
+++ src/sdf_derived.c
@@ -808,6 +808,96 @@ static sdf_block_t *sdf_callback_face_grid(sdf_file_t *h, sdf_block_t *b)
         b->grids = calloc(b->ngrids, sizeof(float*));
     }
 
+    if (old->blocktype == SDF_BLOCKTYPE_LAGRANGIAN_MESH) {
+        int i0, i1, ii0, ii1, nm, *ii, f0, f1;
+        int64_t *nn, irem, j;
+
+        b->nelements = 1;
+        b->nelements_local = 1;
+        for (i = 0; i < b->ndims; i++) {
+            b->dims[i] = old->dims[i];
+            b->local_dims[i] = old->local_dims[i];
+            if (i == b->stagger) {
+                b->dims[i]++;
+                b->local_dims[i]++;
+            }
+            b->nelements *= b->dims[i];
+            b->nelements_local *= b->local_dims[i];
+        }
+
+        sz = b->nelements_local * SDF_TYPE_SIZES[b->datatype_out];
+        for (i = 0; i < b->ndims; i++)
+            b->grids[i] = malloc(sz);
+
+        nn = malloc(b->ndims * sizeof(nn[0]));
+        ii = malloc(b->ndims * sizeof(ii[0]));
+
+        nn[0] = 1;
+        for (i = 1; i < b->ndims; i++)
+            nn[i] = nn[i-1] * b->local_dims[i-1];
+
+        for (i = 0; i < b->nelements_local; i++) {
+            j = b->ndims - 1;
+            irem = i;
+            for (n = 0; n < b->ndims; n++) {
+                ii[j] = irem / nn[j];
+                irem -= ii[j] * nn[j];
+                j--;
+            }
+
+            i0 = i1 = 0;
+            f0 = f1 = nm = 1;
+            for (n = 0; n < b->ndims; n++) {
+                ii0 = ii1 = ii[n];
+                if (n == b->stagger) {
+                    ii0--;
+                    if (ii0 < 0) {
+                        ii0 = 1;
+                        f0 = -1;
+                        f1 = 3;
+                    }
+                    if (ii1 > old->local_dims[n] - 1) {
+                        ii1 = old->local_dims[n] - 2;
+                        f0 = 3;
+                        f1 = -1;
+                    }
+                }
+                i0 += ii0 * nm;
+                i1 += ii1 * nm;
+                nm *= old->local_dims[n];
+            }
+
+            if (b->datatype_out == SDF_DATATYPE_REAL8) {
+                double x0, x1, *x;
+
+                for (n = 0; n < b->ndims; n++) {
+                    x = old->grids[n];
+                    x0 = f0 * x[i0];
+                    x1 = f1 * x[i1];
+                    x = b->grids[n];
+                    x[i] = 0.5 * (x0 + x1);
+                }
+            } else if (b->datatype_out == SDF_DATATYPE_REAL4) {
+                float x0, x1, *x;
+
+                for (n = 0; n < b->ndims; n++) {
+                    x = old->grids[n];
+                    x0 = f0 * x[i0];
+                    x1 = f1 * x[i1];
+                    x = b->grids[n];
+                    x[i] = 0.5 * (x0 + x1);
+                }
+            }
+        }
+
+        free(nn);
+        free(ii);
+
+        b->done_data = 1;
+
+        return b;
+    }
+
     n = b->stagger;
     b->local_dims[n] += 1;
     b->nelements = 0;
