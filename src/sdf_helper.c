44426c78bc217cfed84ba5234ca67e8fb6a490b0
Keith Bennett <K.Bennett@warwick.ac.uk>
2018-03-28 14:24:37 +0100

    Correctly allocate memory for SDF_BLOCKTYPE_CONTIGUOUS
    Memory associated with a block of type SDF_BLOCKTYPE_CONTIGUOUS
    should belong to the top-level block and not the subblocks.
    This change alters sdf_helper_read_data() so that it does the
    right thing.

--- src/sdf_helper.c
+++ src/sdf_helper.c
@@ -17,6 +17,42 @@ int sdf_helper_read_data(sdf_file_t *h, sdf_block_t *b)
     int i;
     sdf_block_t *block;
 
+    if (b->blocktype == SDF_BLOCKTYPE_CONTIGUOUS) {
+        sdf_block_t *var;
+        if (!b->data) {
+            // Free any components which have already been allocated
+            for (int i = 0; i < b->n_ids; i++) {
+                if (b->must_read[i]) {
+                    var = sdf_find_block_by_id(h, b->variable_ids[i]);
+                    // Make sure that contiguous block datatype matches that
+                    // of the stitched blocks
+                    b->datatype     = var->datatype;
+                    b->datatype_out = var->datatype_out;
+                    b->nelements_local = var->nelements_local;
+                    memcpy(b->local_dims, var->local_dims,
+                           var->ndims * sizeof(*b->local_dims));
+                    if (var->data) sdf_stack_free_block(h, var);
+                }
+            }
+            b->done_data = 0;
+            sdf_stack_alloc(h, b);
+        }
+
+        for (int i = 0; i < b->n_ids; i++) {
+            // Fill in derived components which are not already cached
+            if (b->must_read[i]) {
+                var = sdf_find_block_by_id(h, b->variable_ids[i]);
+                if (var) {
+                    var->data = (char*)b->data + i * var->nelements_local
+                            * SDF_TYPE_SIZES[var->datatype_out];
+                    var->dont_own_data = 1;
+                    sdf_helper_read_data(h, var);
+                }
+            }
+        }
+        return 0;
+    }
+
     for (i = 0; i < b->n_ids; i++) {
         // Fill in derived components which are not already cached
         if (b->must_read[i]) {
