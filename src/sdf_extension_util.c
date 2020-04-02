4adbe5c23f7640e60d217aabf8ebefed4e716ddf
Keith Bennett <k.bennett@warwick.ac.uk>
2020-04-02 07:43:03 +0100

    Add full pathname of extension library to info string

--- src/sdf_extension_util.c
+++ src/sdf_extension_util.c
@@ -6,6 +6,13 @@
  * See the LICENSE file for details.
  */
 
+#ifdef __APPLE__
+#  include <mach-o/dyld.h>
+#else // Linux
+#  define _GNU_SOURCE
+#  include <link.h>
+#endif
+#include <dlfcn.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
@@ -23,8 +30,44 @@ static void *sdf_global_extension = NULL;
 static void *sdf_global_extension_dlhandle = NULL;
 static int   sdf_global_extension_failed = 0;
 static int   sdf_global_extension_refcount = 0;
+static char *sdf_global_extension_path = NULL;
 
 int sdf_purge_duplicates(sdf_file_t *h);
+
+
+static const char *dlpath(void *handle)
+{
+    const char *path = NULL;
+
+#ifdef __APPLE__
+    for (int32_t i = _dyld_image_count(); i >= 0 ; i--) {
+
+        bool found = FALSE;
+        const char *probe_path = _dyld_get_image_name(i);
+        void *probe_handle = dlopen(probe_path,
+                                    RTLD_NOW | RTLD_LOCAL | RTLD_NOLOAD);
+
+        if (handle == probe_handle) {
+            found = TRUE;
+            path = probe_path;
+        }
+
+        dlclose(probe_handle);
+
+        if (found)
+            break;
+    }
+#else // Linux
+    struct link_map *map;
+    dlinfo(handle, RTLD_DI_LINKMAP, &map);
+
+    if (map)
+        path = map->l_name;
+#endif
+
+    return path;
+}
+
 
 void *sdf_extension_load(sdf_file_t *h)
 {
@@ -97,8 +140,16 @@ void *sdf_extension_load(sdf_file_t *h)
 
     sdf_global_extension = sdf_extension_create(h);
 
+    sdf_global_extension_path = strdup(dlpath(sdf_global_extension_dlhandle));
+
     return sdf_global_extension;
 #endif
+}
+
+
+char *sdf_extension_path(void)
+{
+    return sdf_global_extension_path;
 }
 
 
@@ -252,12 +303,35 @@ char *sdf_extension_get_info_string(sdf_file_t *h, char const *prefix)
     info = NULL;
 
     if (ext) {
-        info = ext->get_info(ext);
+        int ilen, plen, rlen, len;
+        char *ptr, *oldinfo;
+        static char const *pstr = "Extension path: ";
+
+        oldinfo = ext->get_info(ext);
+        ilen = strlen(oldinfo);
+        plen = strlen(pstr);
+        rlen = strlen(sdf_global_extension_path);
+
+        len = ilen + plen + rlen + 2;
+        ptr = info = malloc(len);
+
+        memcpy(ptr, pstr, ilen);
+        ptr += plen;
+        memcpy(ptr, sdf_global_extension_path, rlen);
+        ptr += rlen;
+        *ptr = '\n';
+        ptr++;
+        memcpy(ptr, oldinfo, ilen);
+        ptr += ilen;
+        free(oldinfo);
+
         if (prefix) {
-            int ilen = strlen(info);
-            int plen = strlen(prefix);
-            int i, rlen, len, count = 1;
-            char *c, *ptr, *oldinfo = info;
+            int i, count = 1;
+            char *c;
+
+            ilen = strlen(info);
+            plen = strlen(prefix);
+            oldinfo = info;
 
             for (i=0, c=oldinfo; i < ilen; i++, c++) {
                 if (*c == '\n')
